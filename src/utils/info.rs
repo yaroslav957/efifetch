dede15a8b5579598d2d0497aa5289d72584954f7
Yaroslav <yaroslav.lobachev9@gmail.com>
2024-06-30 00:17:41 +0300

    Update again

--- src/utils/info.rs
+++ src/utils/info.rs
@@ -1,6 +1,11 @@
-use raw_cpuid::{CpuId, CpuIdReaderNative, HypervisorInfo, ProcessorBrandString, VendorInfo};
+use alloc::string::{String, ToString};
+use core::arch::x86_64::{__cpuid, CpuidResult};
+
 use uefi::prelude::RuntimeServices;
 use uefi::table::runtime::Time;
+
+const VMX_BITMASK: u32 = 1 << 5;
+const SMX_BITMASK: u32 = 1 << 6;
 
 pub(crate) struct Date {
     pub(crate) day: u8,
@@ -9,12 +14,61 @@ pub(crate) struct Date {
 }
 
 pub(crate) struct CpuInfo {
-    pub(crate) brand: ProcessorBrandString,
-    pub(crate) vendor: VendorInfo,
-    pub(crate) hypervisor: HypervisorInfo<CpuIdReaderNative>,
+    pub(crate) brand: String,
+    pub(crate) vendor: String,
+    pub(crate) hypervisor: String,
     pub(crate) vmx: bool,
     pub(crate) smx: bool,
 }
+
+
+#[repr(u32)]
+enum Leaf {
+    Vendor = 0x0,
+    Vmx = 0x1,
+    Hypervisor = 0x40000000,
+    Smx = 0x80000001,
+    Brand = 0x80000002,
+}
+
+#[inline]
+fn vmx_support() -> Option<bool> {
+    let cpuid_result = unsafe { __cpuid(Leaf::Vmx as _) };
+    Some(cpuid_result.ecx & VMX_BITMASK != 0)
+}
+
+#[inline]
+fn smx_support() -> Option<bool> {
+    let cpuid_result = unsafe { __cpuid(Leaf::Smx as _) };
+    Some(cpuid_result.ecx & SMX_BITMASK != 0)
+}
+
+#[inline]
+fn get_cpuid_info(buffer: &mut [u8; 12], cpuid: Leaf) {
+    unsafe {
+        let CpuidResult { ebx, ecx, edx, .. } = __cpuid(cpuid as _);
+        let cpuid_result = [ebx, ecx, edx];
+        for i in 0..3 {
+            let name_slice = &mut buffer[4 * i..4 * (i + 1)];
+            name_slice.copy_from_slice(&cpuid_result[i].to_le_bytes());
+        }
+    }
+}
+
+#[inline]
+fn getinfo(buffer: &mut [u8; 48]) {
+    unsafe {
+        for i in 0..3_usize {
+            let CpuidResult { ebx, ecx, edx, .. } = __cpuid(Leaf::Brand as u32 + i as u32);
+            let cpuid_result = [ebx, ecx, edx];
+            for j in 0..4 {
+                let name_slice = &mut buffer[16 * i..][4 * j..][0..4];
+                name_slice.copy_from_slice(&cpuid_result[j].to_le_bytes());
+            }
+        }
+    }
+}
+
 
 impl From<Time> for Date {
     fn from(time: Time) -> Self {
@@ -35,20 +89,28 @@ impl Date {
 
 impl CpuInfo {
     pub(crate) fn get() -> Self {
-        let cpu = CpuId::new();
+        let mut hypervisor_buff = [0u8; 12];
+        let mut vendor_buff = [0u8; 12];
+        let mut brand_buff = [0u8; 48];
+        get_cpuid_info(&mut hypervisor_buff, Leaf::Hypervisor);
+        get_cpuid_info(&mut vendor_buff, Leaf::Vendor);
+        getinfo(&mut brand_buff);
+
         Self {
-            brand: cpu.get_processor_brand_string()
-                .expect("Cant get brand info"),
-            vendor: cpu.get_vendor_info()
-                .expect("Cant get vendor info"),
-            hypervisor: cpu.get_hypervisor_info()
-                .expect("Cant get hypervisor info"),
-            vmx: cpu.get_feature_info()
-                .expect("Cant get vmx info")
-                .has_vmx(),
-            smx: cpu.get_feature_info()
-                .expect("Cant get smx info")
-                .has_smx()
+            brand: core::str::from_utf8(&brand_buff)
+                .expect("Cant get vendor info")
+                .trim_matches('\0')
+                .to_string(),
+            vendor: core::str::from_utf8(&vendor_buff)
+                .expect("Cant get vendor info")
+                .to_string(),
+            hypervisor: core::str::from_utf8(&hypervisor_buff)
+                .expect("Cant get hypervisor info")
+                .to_string(),
+            vmx: vmx_support()
+                .expect("Cant get vmx info"),
+            smx: smx_support()
+                .expect("Cant get smx info"),
         }
     }
 }
